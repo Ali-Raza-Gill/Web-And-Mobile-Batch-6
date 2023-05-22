871af3ae5a477a661a236f8ff080ed4b74b801bd
Ali Raza <aliraza137725@gmail.com>
2023-05-22 11:41:51 +0500

    Javascript material added

---
+++ Javascript/Assignments/Assignment 10/Practice.js
@@ -0,0 +1,31 @@
+// Date handling
+var date = new Date();
+// console.log(date);
+
+
+// 1. Write a JavaScript function to check whether an `input` is a date object or not.   
+
+// console.log(is_date("oct 13, 2000 11:12:33"));//true
+// console.log(is_date(new date(49999)));//false
+// console.log(is_date(new date(99,99,98,90)));//false
+// console.log(is_date([1,3,4,5]));//false
+
+//  2. Write a JavaScript function to get the current date.
+
+a=date.getDate();
+b=date.getDay();
+c=date.getFullYear();
+d=date.getHours();
+e=date.getMonth();
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
+// console.log(e);
+
+console.log();
+
+
+
+
---
+++ Javascript/Assignments/Assignment 12 Array handling/practice.js
@@ -0,0 +1,92 @@
+// Assignment no 12 Array Handling
+
+
+// 1. Write a js program to find length of a string.
+var string="abcdefghijklmnopqrstuvwxyz";
+var str="M.aliraza";
+
+// var len=string.length;
+// var length=str.length;
+// console.log(len);
+// console.log(length);
+
+
+// 2. Write a js program to copy one string to another string.
+// const concat=string+" "+str;
+// console.log(concat);
+
+// 3. Write a js program to concatenate two strings.
+// const concat=string+" "+str;
+// console.log(concat);
+
+
+// 4. Write a js program to compare two strings.
+// if(string.length==str.length){
+//     console.log("both are equal");
+// }
+// else{
+//     console.log("both are diffrent");
+// }
+
+// 5. Write a js program to convert lowercase string to uppercase.
+// const lowertoupper=string.toUpperCase();
+// console.log(lowertoupper);
+// 6. Write a js program to convert uppercase string to lowercase.
+// const uppertolower=string.toLowerCase();
+// console.log(uppertolower);
+
+// 7. Write a js program to toggle case of each character of a string.// it generate error
+// const originalString = "Hello World!";
+// const toggledString = toggleCase(originalString);
+// console.log(toggledString);
+
+
+// 8. Write a js program to find total number of alphabets, digits or special character in a string.
+// function countCharacters(str) {
+//     let alphabets = 0, digits = 0, special = 0;
+    
+//     for (let i = 0; i < str.length; i++) {
+//       const char = str[i];
+//       if (/[a-zA-Z]/.test(char)) {
+//         alphabets++;
+//       } else if (/[0-9]/.test(char)) {
+//         digits++;
+//       } else {
+//         special++;
+//       }
+//     }
+    
+//     return { alphabets, digits, special };
+//   }
+//   console.log(countCharacters);
+
+
+// Write a js program to count total number of vowels and consonants in a string.
+
+// function countvowelsandconsonants(string){
+//     let vowels =0, consonants=0;
+//     for (let index = 0; index < string.length; index++) {
+//         const char = string[index].tolowecase();
+//         if (/[aeiou]/.test(char)) {
+//             vowels++;
+//         }else if(/[a-z]/.test(char)){
+//             consonants++;
+//         }
+        
+//     }  
+//     return{ vowels, consonants};
+// }
+// const vccount=countvowelsandconsonants(string);
+// console.log(vccount);
+
+
+// 10. Write a js program to count total number of words in a string.
+
+
+
+
+
+
+
+
+
